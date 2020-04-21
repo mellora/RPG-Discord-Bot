c1651895bfddb1d141e348a11fa1b0b8675b30e9
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-21 12:19:06 -0500

    Added Command Context functionality

--- src/main/java/com/mellora/rpgbot/bot/command/CommandContext.java
+++ src/main/java/com/mellora/rpgbot/bot/command/CommandContext.java
@@ -1,5 +1,33 @@
 package com.mellora.rpgbot.bot.command;
 
-public class CommandContext {
+import java.util.List;
+
+import me.duncte123.botcommons.commands.ICommandContext;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+
+public class CommandContext implements ICommandContext {
+
+	private final GuildMessageReceivedEvent event;
+	private final List<String> args;
+
+	public CommandContext(GuildMessageReceivedEvent event, List<String> args) {
+		this.event = event;
+		this.args = args;
+	}
+
+	@Override
+	public Guild getGuild() {
+		return this.getEvent().getGuild();
+	}
+
+	@Override
+	public GuildMessageReceivedEvent getEvent() {
+		return this.event;
+	}
+
+	public List<String> getArgs() {
+		return this.args;
+	}
 
 }
