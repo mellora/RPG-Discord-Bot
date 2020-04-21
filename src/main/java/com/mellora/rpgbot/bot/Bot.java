65c288e8b2fcca97161491910bc71cfd4931bc9a
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-21 12:05:41 -0500

    Program now retrieves token from json file

--- src/main/java/com/mellora/rpgbot/bot/Bot.java
+++ src/main/java/com/mellora/rpgbot/bot/Bot.java
@@ -1,17 +1,27 @@
 package com.mellora.rpgbot.bot;
 
-import org.springframework.beans.factory.annotation.Value;
+import java.io.FileReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import net.dv8tion.jda.api.JDABuilder;
 
 @Component
 public class Bot {
-	@Value("${bot.token}")
 	private String token;
 
 	public Bot() throws Exception {
-		System.out.println(token);
-//		JDABuilder.createDefault(token).build();
+		// Create Resource Object from resources folder
+		Resource res = new ClassPathResource("config/config.json");
+		// Creates a JSON Object from the Resource Object
+		JSONObject jsonObj = (JSONObject) new JSONParser().parse(new FileReader(res.getFile()));
+		// Assigns the bot token from the config file
+		token = (String) jsonObj.get("token");
+		// Initializes the Bot using the token provided.
+		JDABuilder.createDefault(token).build();
 	}
 }
