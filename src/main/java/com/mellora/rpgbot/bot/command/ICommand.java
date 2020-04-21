a9c785fd29443bd8296a6202f535e651c0cd6d28
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-21 12:11:00 -0500

    Added interface methods

--- src/main/java/com/mellora/rpgbot/bot/command/ICommand.java
+++ src/main/java/com/mellora/rpgbot/bot/command/ICommand.java
@@ -1,5 +1,15 @@
 package com.mellora.rpgbot.bot.command;
 
-public interface ICommand {
+import java.util.List;
 
+public interface ICommand {
+	void handle(CommandContext ctx);
+	
+	String getName();
+	
+	String getHelp();
+	
+	default List<String> getAliases() {
+		return List.of();  // Introduced in Java 9, Use Arrays.asList on Java 8
+	}
 }
