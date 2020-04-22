ac85180e2dfc325df393fe617fea83a68028eeef
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-22 13:40:18 -0500

    Incremented the returned value by one for human readability.

--- src/main/java/com/mellora/rpgbot/bot/service/DiceRoller.java
+++ src/main/java/com/mellora/rpgbot/bot/service/DiceRoller.java
@@ -3,6 +3,7 @@ package com.mellora.rpgbot.bot.service;
 import java.util.Random;
 
 public class DiceRoller {
+
 	Random random = new Random();
 
 	public DiceRoller() {
@@ -10,30 +11,30 @@ public class DiceRoller {
 	}
 
 	public int rollD4() {
-		return random.nextInt(4);
+		return random.nextInt(4) + 1;
 	}
 
 	public int rollD6() {
-		return random.nextInt(6);
+		return random.nextInt(6) + 1;
 	}
 
 	public int rollD8() {
-		return random.nextInt(8);
+		return random.nextInt(8) + 1;
 	}
 
 	public int rollD10() {
-		return random.nextInt(10);
+		return random.nextInt(10) + 1;
 	}
 
 	public int rollD12() {
-		return random.nextInt(12);
+		return random.nextInt(12) + 1;
 	}
 
 	public int rollD20() {
-		return random.nextInt(20);
+		return random.nextInt(20) + 1;
 	}
 
 	public int rollD100() {
-		return random.nextInt(100);
+		return random.nextInt(100) + 1;
 	}
 }
