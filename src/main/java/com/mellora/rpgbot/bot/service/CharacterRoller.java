73d9c6b50ffbdc2f8a0eafe419c7654c48af6160
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-22 14:46:33 -0500

    Removed excess code and switched from rolling d4 to d6

--- src/main/java/com/mellora/rpgbot/bot/service/CharacterRoller.java
+++ src/main/java/com/mellora/rpgbot/bot/service/CharacterRoller.java
@@ -1,7 +1,7 @@
 package com.mellora.rpgbot.bot.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class CharacterRoller {
@@ -17,12 +17,9 @@ public class CharacterRoller {
 	public List<Integer> simpleCharacter() {
 		List<Integer> list = new ArrayList<>();
 		for(int x = 0; x < 6; x++) {
-			List<Integer> temp = multiDiceRoller.rollD4(4);
-			System.out.println(temp.toString());
-			Arrays.sort(temp.toArray());
-			System.out.println(temp.toString());
+			List<Integer> temp = multiDiceRoller.rollD6(4);
+			Collections.sort(temp);
 			temp.remove(0);
-			System.out.println(temp.toString());
 			list.add(temp.stream().mapToInt(Integer::intValue).sum());
 		}
 		return list;
