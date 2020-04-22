fcb87a269ca0bd4c30d280edecebfc4e83309aa8
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-22 13:14:00 -0500

    Cleaned up code

--- src/main/java/com/mellora/rpgbot/bot/Listener.java
+++ src/main/java/com/mellora/rpgbot/bot/Listener.java
@@ -5,7 +5,6 @@ import javax.annotation.Nonnull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.context.annotation.Configuration;
 
 import com.mellora.rpgbot.Config;
 import com.mellora.rpgbot.DiscordJavaBotApplication;
@@ -18,28 +17,28 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 public class Listener extends ListenerAdapter {
-	
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
-	
+
 	private final CommandManager manager = new CommandManager();
-	
+
 	@Override
 	public void onReady(@Nonnull ReadyEvent event) {
-		
+		LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
 	}
-	
+
 	@Override
 	public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
 		User user = event.getAuthor();
-		
-		if(user.isBot() || event.isWebhookMessage()) {
+
+		if (user.isBot() || event.isWebhookMessage()) {
 			return;
 		}
-		
+
 		String prefix = Config.get("default_prefix");
 		String raw = event.getMessage().getContentRaw();
-		
-		if(raw.equalsIgnoreCase(prefix + "shutdown") && user.getId().equals(Config.get("owner_id"))) {
+
+		if (raw.equalsIgnoreCase(prefix + "shutdown") && user.getId().equals(Config.get("owner_id"))) {
 			event.getChannel().sendMessage("Shutting Down.").queue();
 			LOGGER.info("Shutting Down");
 			event.getJDA().shutdown();
@@ -47,8 +46,8 @@ public class Listener extends ListenerAdapter {
 			SpringApplication.exit(DiscordJavaBotApplication.ctx, () -> 0);
 			return;
 		}
-		
-		if(raw.startsWith(prefix)) {
+
+		if (raw.startsWith(prefix)) {
 			manager.handle(event);
 		}
 	}
