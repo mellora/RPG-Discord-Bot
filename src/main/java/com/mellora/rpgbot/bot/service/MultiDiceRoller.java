09d0127270a99ab3948c6a90747ab7b3130dfc81
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-22 13:53:28 -0500

    Created Service Class that Will roll multiple dice of the same type.

---
+++ src/main/java/com/mellora/rpgbot/bot/service/MultiDiceRoller.java
@@ -0,0 +1,69 @@
+package com.mellora.rpgbot.bot.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MultiDiceRoller {
+	
+	DiceRoller roller;
+	
+	public MultiDiceRoller(){
+		roller = new DiceRoller();
+	}
+	
+	public List<Integer> rollD4(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD4());
+		}
+		return diceList;
+	}
+	
+	public List<Integer> rollD6(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD6());
+		}
+		return diceList;
+	}
+	
+	public List<Integer> rollD8(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD8());
+		}
+		return diceList;
+	}
+	
+	public List<Integer> rollD10(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD10());
+		}
+		return diceList;
+	}
+	
+	public List<Integer> rollD12(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD12());
+		}
+		return diceList;
+	}
+	
+	public List<Integer> rollD20(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD20());
+		}
+		return diceList;
+	}
+	
+	public List<Integer> rollD100(int numberOfDice) {
+		List<Integer> diceList = new ArrayList<>();
+		for (int x = 0; x < numberOfDice; x++) {
+			diceList.add(roller.rollD100());
+		}
+		return diceList;
+	}
+}
