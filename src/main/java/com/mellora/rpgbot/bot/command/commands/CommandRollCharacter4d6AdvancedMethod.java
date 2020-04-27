1194c15971e9f54b8d2f6145a2f43f3b6a418a09
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-27 14:32:20 -0500

    Started Command Class for new character creation route

---
+++ src/main/java/com/mellora/rpgbot/bot/command/commands/CommandRollCharacter4d6AdvancedMethod.java
@@ -0,0 +1,23 @@
+package com.mellora.rpgbot.bot.command.commands;
+
+import com.mellora.rpgbot.bot.command.CommandContext;
+import com.mellora.rpgbot.bot.command.ICommand;
+
+public class CommandRollCharacter4d6AdvancedMethod implements ICommand {
+
+	@Override
+	public void handle(CommandContext ctx) {
+		
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Override
+	public String getHelp() {
+		return null;
+	}
+
+}
