6b588ab83a73ee7de0ac84735b47759b47c37108
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-27 16:40:22 -0500

    Started to implement a new way to re-roll ones

--- src/main/java/com/mellora/rpgbot/bot/service/CharacterRoller.java
+++ src/main/java/com/mellora/rpgbot/bot/service/CharacterRoller.java
@@ -29,12 +29,21 @@ public class CharacterRoller {
 		List<Integer> list = new ArrayList<>();
 		for (int x = 0; x < 6; x++) {
 			List<Integer> temp = multiDiceRoller.rollD6(4);
+			System.out.println("List " + x + ": " + temp);
 			while (temp.contains(1)) {
-				Collections.replaceAll(temp, 1, multiDiceRoller.roller.rollD6());
+				int numberOfOnes = Collections.frequency(list, 1);
+				System.out.println(numberOfOnes);
+				List<Integer> newTemp = multiDiceRoller.rollD6(numberOfOnes);
+				System.out.println(newTemp);
+				temp.removeAll(Collections.singleton(1));
+				temp.addAll(newTemp);
+//				Collections.replaceAll(temp, 1, multiDiceRoller.roller.rollD6());
 			}
+			System.out.println("List " + x + ": " + temp);
 			Collections.sort(temp);
 			temp.remove(0);
 			list.add(temp.stream().mapToInt(Integer::intValue).sum());
+			System.out.println("");
 		}
 		return list;
 	}
