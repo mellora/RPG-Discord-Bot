4af2749154a2867a0ea9aeebeb9a5387d9f0f3e7
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-28 16:27:22 -0500

    Switched to using lombok

--- src/main/java/com/mellora/rpgbot/bot/Listener.java
+++ src/main/java/com/mellora/rpgbot/bot/Listener.java
@@ -2,16 +2,14 @@ package com.mellora.rpgbot.bot;
 
 import javax.annotation.Nonnull;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 
 import com.mellora.rpgbot.Config;
 import com.mellora.rpgbot.DiscordJavaBotApplication;
 
+import lombok.extern.slf4j.Slf4j;
 import me.duncte123.botcommons.BotCommons;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.entities.MessageActivity.Application;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -20,10 +18,8 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  * This class creates a listener used by the bot class to register when a command is
  * sent within discord.
  */
+@Slf4j
 public class Listener extends ListenerAdapter {
-
-	// Allows this class to use the logger set up by Spring Boot.
-	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	// Creates an instance of the manager class to handle commands.
 	private final CommandManager manager = new CommandManager();
@@ -31,38 +27,39 @@ public class Listener extends ListenerAdapter {
 	// Method prints to logger when the bot is ready on Discord.
 	@Override
 	public void onReady(@Nonnull ReadyEvent event) {
-		LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
+		log.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
 	}
 
 	// Method fires when an event is registered on a Discord server.
 	@Override
 	public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
-		// Creates a user object to help with figuring out if the bot needs to do anything.
+		// Creates a user object to help with figuring out if the bot needs to do
+		// anything.
 		User user = event.getAuthor();
 
 		/*
-		 *  Checks if the event creator us a bot account or a webhook message.
-		 *  This prevents the bot from spending time on unnecessary work by
-		 *  returning from method call.
+		 * Checks if the event creator us a bot account or a webhook message. This
+		 * prevents the bot from spending time on unnecessary work by returning from
+		 * method call.
 		 */
 		if (user.isBot() || event.isWebhookMessage()) {
 			return;
 		}
 
 		// Gets the prefix from config file
-		String prefix = Config.get("default_prefix");  // Work on switching to database
+		String prefix = Config.get("default_prefix"); // Work on switching to database
 		// Gets the content of the event message
 		String raw = event.getMessage().getContentRaw();
 
 		/*
-		 * Checks if the message has the correct guild prefix and sent a shutdown command.
-		 * Then checks if the user is the owner before running code block.
+		 * Checks if the message has the correct guild prefix and sent a shutdown
+		 * command. Then checks if the user is the owner before running code block.
 		 */
 		if (raw.equalsIgnoreCase(prefix + "shutdown") && user.getId().equals(Config.get("owner_id"))) {
 			// Sends shutdown message to the channel that spawned the event.
 			event.getChannel().sendMessage("Shutting Down.").queue();
 			// Sends to logger shutdown message.
-			LOGGER.info("Shutting Down");
+			log.info("Shutting Down");
 			// Sends shutdown command to the Discord bot instance.
 			event.getJDA().shutdown();
 			// Sends kill command to application bot threads.
@@ -72,7 +69,8 @@ public class Listener extends ListenerAdapter {
 			return;
 		}
 
-		// Checks if message has correct guild prefix and sends command to manager if it does.
+		// Checks if message has correct guild prefix and sends command to manager if it
+		// does.
 		if (raw.startsWith(prefix)) {
 			manager.handle(event);
 		}
