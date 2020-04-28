29c3ca98f03cea67a9d5655c93a17fa6bf818a56
Alexander Mellor <AlexMellor2014@gmail.com>
2020-04-28 15:45:50 -0500

    Added Comments for code understanding

--- src/main/java/com/mellora/rpgbot/bot/command/commands/CommandHelp.java
+++ src/main/java/com/mellora/rpgbot/bot/command/commands/CommandHelp.java
@@ -9,48 +9,63 @@ import com.mellora.rpgbot.bot.command.ICommand;
 
 import net.dv8tion.jda.api.entities.TextChannel;
 
-public class CommandHelp implements ICommand{
+/*
+ * Command class for Help command.
+ */
+public class CommandHelp implements ICommand {
 
 	private final CommandManager manager;
-	
+
 	public CommandHelp(CommandManager manager) {
 		this.manager = manager;
 	}
-	
+
+	// Method handles the command logic.
 	@Override
 	public void handle(CommandContext ctx) {
+		
+		// Gets needed values from Command Context.
 		List<String> args = ctx.getArgs();
 		TextChannel channel = ctx.getChannel();
-		
-		if(args.isEmpty()) {
+
+		// Checks if args is empty and outputs all command names to guild if empty.
+		if (args.isEmpty()) {
 			StringBuilder builder = new StringBuilder();
 			builder.append("List of commands\n");
-			manager.getCommands().stream().map(ICommand::getName).forEach((it) -> builder.append("`").append(Config.get("default_prefix")).append(it).append("`\n"));
+			manager.getCommands().stream().map(ICommand::getName)
+					.forEach((it) -> builder.append("`").append(Config.get("default_prefix")).append(it).append("`\n"));
 			channel.sendMessage(builder.toString()).queue();
 			return;
 		}
-		
+
+		// Gets command to search for from args.
 		String search = args.get(0);
 		ICommand command = manager.getCommand(search);
-		
-		if(command == null) {
+
+		// Sends that command doesn't exist if search returns null value.
+		if (command == null) {
 			channel.sendMessage("Nothing found for " + search).queue();
 			return;
 		}
-		
+
+		// Sends the help context for searched for command.
 		channel.sendMessage(command.getHelp()).queue();
 	}
 
+	// Method returns the command name for Discord.
 	@Override
 	public String getName() {
 		return "help";
 	}
 
+	// Method returns the commands help context.
 	@Override
 	public String getHelp() {
-		return "Shows the list with commands in the bot\nUsage: " + Config.get("default_prefix") + this.getName() + " [command]";
+		return "Shows the list with commands in the bot\nUsage: " + Config.get("default_prefix") + this.getName()
+				+ " [command]";
 	}
 
+	// Method returns all other ways to refer to this command.
 	@Override
 	public List<String> getAliases() {
 		return List.of("commands", "cmds", "commandlist");
--- src/main/java/com/mellora/rpgbot/bot/command/commands/CommandRollCharacter4d6AdvancedMethod.java
+++ src/main/java/com/mellora/rpgbot/bot/command/commands/CommandRollCharacter4d6AdvancedMethod.java
@@ -15,29 +15,40 @@ public class CommandRollCharacter4d6AdvancedMethod implements ICommand {
 	CharacterRoller characterRoll;
 	EmbedBuilder eb;
 
+	// Method handles the command logic.
 	@Override
 	public void handle(CommandContext ctx) {
+
+		// Initializes the Character Roller Class Object.
 		characterRoll = new CharacterRoller();
+		// Initializes a new EmbedBuilder for output.
 		eb = new EmbedBuilder();
 
+		// Initializes a list of all scores to be output.
 		List<Integer> rolls = characterRoll.roll4d6DropLowestDieReroll1ForScores();
 
+		// Sets the embed title.
 		eb.setTitle("Character Scores for " + ctx.getAuthor().getName());
 
+		// Sets the embed color.
 		eb.setColor(Color.PINK);
 
+		// Loops through all the rolls and add the roll score to an embed field.
 		for (int x = 1; x <= rolls.size(); x++) {
 			eb.addField("Score " + x + ": ", rolls.get(x - 1).toString(), true);
 		}
 
+		// Sends embed to channel of the guild who used the command.
 		ctx.getChannel().sendMessage(eb.build()).queue();
 	}
 
+	// Method returns the command name for Discord.
 	@Override
 	public String getName() {
 		return "rollCharacter-4d6-advanced";
 	}
 
+	// Method returns the commands help context.
 	@Override
 	public String getHelp() {
 		return "Rolls 6 dice scores of 4 6-sided dice rerolling 1's and dropping the lowest die role for each\nUsage: "
