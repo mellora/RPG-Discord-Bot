8f466aab868ed0416ef4d66076c0bb307654a76a
Alexander Mellor <AlexMellor2014@gmail.com>
2020-05-12 15:56:31 -0500

    Added getTemplate method to interface

--- src/main/java/com/mellora/rpgbot/dao/GuildSettingsDAO.java
+++ src/main/java/com/mellora/rpgbot/dao/GuildSettingsDAO.java
@@ -2,10 +2,14 @@ package com.mellora.rpgbot.dao;
 
 import java.util.List;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.mellora.rpgbot.dao.models.GuildSettings;
 
 public interface GuildSettingsDAO {
 
+	public JdbcTemplate getTemplate();
+	
 	// Create
 	public Integer save(Long guildId);
 
