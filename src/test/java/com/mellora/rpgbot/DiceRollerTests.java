a1efd994b9a93b1cb7f47b883953abed036f841c
Alexander Mellor <AlexMellor2014@gmail.com>
2020-05-19 15:10:46 -0500

    Got Tests working

--- src/test/java/com/mellora/rpgbot/DiceRollerTests.java
+++ src/test/java/com/mellora/rpgbot/DiceRollerTests.java
@@ -4,53 +4,54 @@ import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.Matchers.greaterThanOrEqualTo;
 import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.mock;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 
 import com.mellora.rpgbot.service.dice.DiceRoller;
 
 class DiceRollerTests {
 
-	private static DiceRoller roller = mock(DiceRoller.class);
+	private static DiceRoller roller = new DiceRoller();
+	
+	private static final int numberOfTests = 5;
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD4() {
 		int roll = roller.rollD4();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(4)));
 	}
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD6() {
 		int roll = roller.rollD6();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(6)));
 	}
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD8() {
 		int roll = roller.rollD8();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(8)));
 	}
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD10() {
 		int roll = roller.rollD10();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(10)));
 	}
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD12() {
 		int roll = roller.rollD12();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(12)));
 	}
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD20() {
 		int roll = roller.rollD20();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(20)));
 	}
 
-	@Test
+	@RepeatedTest(value = numberOfTests)
 	void testRollD100() {
 		int roll = roller.rollD100();
 		assertThat(roll, allOf(greaterThanOrEqualTo(1), lessThanOrEqualTo(100)));
