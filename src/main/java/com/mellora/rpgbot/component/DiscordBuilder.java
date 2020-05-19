0f15cc7d22034f1175bb6f142663d603a0f60702
Alexander Mellor <AlexMellor2014@gmail.com>
2020-05-19 11:20:54 -0500

    Formatting

--- src/main/java/com/mellora/rpgbot/component/DiscordBuilder.java
+++ src/main/java/com/mellora/rpgbot/component/DiscordBuilder.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DiscordBuilder {
-	
+
 	public DiscordBuilder(@Value("${discord.bot.token}") String botToken) {
 		System.out.println(botToken);
 	}
