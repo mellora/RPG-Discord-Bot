7ebe144537def157cae6d33e3137490a4fa4ca39
Alexander Mellor <AlexMellor2014@gmail.com>
2020-05-19 13:37:54 -0500

    Added query

--- src/main/java/com/mellora/rpgbot/dao/GuildSettingsRepository.java
+++ src/main/java/com/mellora/rpgbot/dao/GuildSettingsRepository.java
@@ -1,7 +1,11 @@
 package com.mellora.rpgbot.dao;
 
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface GuildSettingsRepository extends CrudRepository<GuildSettings, Integer>{
 
+	@Query(value = "SELECT * FROM guild_setting WHERE guild_id = :guild_id")
+	GuildSettings getGuildByGuildId(@Param(value = "guild_id") Long guildId);
 }
