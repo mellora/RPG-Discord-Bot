5b917b2ce5f173ac993c37a45a058fe6b1dccc16
Alexander Mellor <AlexMellor2014@gmail.com>
2020-05-27 17:25:44 -0500

    Started to set up new command to change command prefix

---
+++ src/main/java/com/mellora/rpgbot/service/command/commands/SetPrefixCommand.java
@@ -0,0 +1,58 @@
+package com.mellora.rpgbot.service.command.commands;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mellora.rpgbot.dao.GuildSettingsRepository;
+import com.mellora.rpgbot.service.command.CommandContext;
+import com.mellora.rpgbot.service.command.ICommand;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+@Service
+public class SetPrefixCommand implements ICommand {
+
+	@Autowired
+	private GuildSettingsRepository repo;
+	
+	private String prefix;
+	
+	@Override
+	public void handle(CommandContext ctx) {
+		
+		final TextChannel channel = ctx.getChannel();
+		final List<String> args = ctx.getArgs();
+		final Member member = ctx.getMember();
+		
+		if(!member.hasPermission(Permission.MANAGE_SERVER)) {
+			channel.sendMessage("You do not have the power to manage this server.").queue();
+			return;
+		}
+		
+		if (args.isEmpty()) {
+			channel.sendMessage("Missing Args").queue();
+			return;
+		}
+		
+		final String newPrefix = String.join("", args);
+		
+		channel.sendMessageFormat("New prefix has been set to `%s`", newPrefix);
+
+	}
+
+	@Override
+	public String getName() {
+		return "setPrefix";
+	}
+
+	@Override
+	public String getHelp() {
+		return "Sets the prefix for this server.\nUsage: " + prefix + this.getName()
+		+ " <prefix>";
+	}
+
+}
